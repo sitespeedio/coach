9f0ad2c94be99b016e9b4cfef287e9cf14e66a6e
soulgalore <peter@soulgalore.com>
2016-02-05 09:21:50 +0100

    better redirect checks

--- test/har/performance/assetsRedirectsTest.js
+++ test/har/performance/assetsRedirectsTest.js
@@ -7,7 +7,7 @@ describe('Avoid redirects for the assets', function() {
 
   it('We should be able to find redirects on assets', function() {
     return har.getHARresult('test/har/files/assetsRedirects.har').then((result) => {
-      assert.strictEqual(result[0].performance.assetsRedirects.offending.length, 1);
+      assert.strictEqual(result[0].performance.assetsRedirects.offending.length, 44);
       assert.strictEqual(result[0].performance.assetsRedirects.score < 100, true);
     });
   });
