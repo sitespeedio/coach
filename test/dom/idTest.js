4a3d0790bdfc79b554e0768ae836578fb61cdec6
Tobias Lidskog <tobias@lidskog.se>
2016-02-15 09:35:05 +0100

    Fix id/filename mismatch + add test for match.

---
+++ test/dom/idTest.js
@@ -0,0 +1,31 @@
+'use strict';
+const Promise = require('bluebird'),
+  assert = require('assert'),
+  fs = require('fs'),
+  path = require('path');
+
+Promise.promisifyAll(fs);
+
+describe('Verify advice ids ', function() {
+  it('should have an id that matches the file name ', function() {
+    const domAdviceDir = path.join(__dirname, '..', '..', 'lib', 'dom');
+    const adviceCategoriesWithIds = ['accessibility', 'bestpractice', 'performance'];
+    return fs.readdirAsync(domAdviceDir)
+      .filter((filename) => fs.statAsync(path.join(domAdviceDir, filename))
+        .then((stat) => stat.isDirectory()))
+      .filter((dirname) => adviceCategoriesWithIds.indexOf(dirname) !== -1)
+      .each((dirname) => {
+        return fs.readdirAsync(path.join(domAdviceDir, dirname))
+          .filter((filename) => filename.endsWith('.js'))
+          .each((filename) => {
+            const name = filename.slice(0, -3);
+
+            return fs.readFileAsync(path.join(domAdviceDir, dirname, filename), 'utf8')
+              .then((contents) => {
+                const id = contents.match(/id: '([^\']*)',/)[1];
+                assert(id === name, 'Mismatch of id/filename for ' + filename);
+              })
+          })
+      })
+  });
+});
