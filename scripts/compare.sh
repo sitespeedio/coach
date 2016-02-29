1d5e5490a4f0b90f117c7ca2c1d0d979645a197b
soulgalore <peter@soulgalore.com>
2016-02-29 23:47:17 +0100

    save to file

--- scripts/compare.sh
+++ scripts/compare.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+export LANG=C.UTF-8
 
 if [ $# -ne 1 ]; then
     echo $0: usage: compare urls.txt
@@ -13,12 +14,12 @@ URLS=()
 while read txt ; do
    URLS[${#URLS[@]}]=$txt
 done < $input
-echo 'URL, Coach, GPSI, YSlow' > result.txt
+echo 'URL, Coach, GPSI Desktop, YSlow' > result.txt
 
 for url in "${URLS[@]}"; do
 echo "$url"
 gpsi="$(gpagespeed "$url" --key $GPSI_KEY | jq .ruleGroups.SPEED.score)"
-coach="$(webcoach -u "$url" | jq .performance.score)"
+coach="$(webcoach -u "$url" > apa.txt && cat apa.txt | jq .performance.score)"
 yslow="$(phantomjs yslow.js "$url" | jq .o )"
 echo "$url, $coach, $gpsi, $yslow" >> result.txt
 done
