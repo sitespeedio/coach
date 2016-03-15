f5d5aa4612078e4d9901f284a531ab2200afc6ad
soulgalore <peter@soulgalore.com>
2016-03-15 21:08:50 +0100

    make the cli easier to test, part 2

--- bin/webcoach.js
+++ bin/webcoach.js
@@ -2,69 +2,11 @@
 
 'use strict';
 
-let fs = require('fs'),
-  path = require('path'),
-  yargs = require('yargs'),
-  Promise = require('bluebird'),
-  stringify = require('json-stable-stringify'),
-  api = require('../'),
-  packageInfo = require('../package');
+let stringify = require('json-stable-stringify'),
+  cli = require('../lib/cli');
 
-Promise.promisifyAll(fs);
-var cli = {
-  runDOMAndHar: function(options) {
-    var browsertime = api.runDomAdvice(options.url, api.getDomAdvice(), {
-      browser: options.browser,
-      experimental: {
-        nativeHar: true
-      },
-      iterations: 1
-    });
 
-    var har = browsertime.then((result) => api.runHarAdvice(result.har, api.getHarAdvice()));
-
-    return Promise.join(browsertime, har, function(browsertimeResult, harResult) {
-      let total = api.merge(browsertimeResult.browsertimeData[0].coach, harResult);
-      return total;
-    });
-  },
-
-  runHAR: function(options) {
-    return fs.readFileAsync(path.resolve(process.cwd(), options.file))
-      .then(JSON.parse)
-      .then((har) => api.runHarAdvice(har, api.getHarAdvice()));
-  }
-};
-
-let options = yargs
-  .usage('$0 [options]')
-  .version(() => `${packageInfo.name} ${packageInfo.version}`)
-  .alias('V', 'version')
-  .help('h')
-  .alias('h', 'help')
-  .option('b', {
-    describe: 'Browser to run (only applicable with --url).',
-    default: 'firefox',
-    alias: 'browser'
-  })
-  .option('u', {
-    describe: 'Url to run in browser and analyze DOM for advice.',
-    alias: 'url'
-  })
-  .option('f', {
-    describe: 'HAR file to parse and analyze for advice.',
-    alias: 'file'
-  })
-  .wrap(yargs.terminalWidth())
-  .check((argv) => {
-    if (!(argv.u || argv.f)) {
-      return '-u or -b must be specified.';
-    }
-    return true;
-  })
-  .strict()
-  .argv;
-
+let options = cli.getOptions();
 
 if (options.url) {
   cli.runDOMAndHar(options).then((result) => {
@@ -87,5 +29,3 @@ if (options.url) {
     process.exit(1);
   });
 }
-
-module.exports = cli;
