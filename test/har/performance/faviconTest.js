127e740bd1a807ed4c9be2d73afa3d16c5f6680b
soulgalore <peter@soulgalore.com>
2016-03-22 08:21:31 +0100

    run favicon test as chrome only since it's disabled in firefox

--- test/har/performance/faviconTest.js
+++ test/har/performance/faviconTest.js
@@ -11,7 +11,8 @@ describe('Use favicon', function() {
   });
 
   it('We should be able to know if we we are missing favicon', function() {
-    return har.getHARresult('test/har/files/favicon2.har').then((result) => {
+    // The favicon advice only work correct for Chrome
+    return har.getHARresult('test/har/files/favicon2.har', undefined, {browser: 'chrome'}).then((result) => {
       assert.strictEqual(result[0].coachAdvice.results.performance.adviceList.favicon.score, 0);
     });
   });
