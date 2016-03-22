20118e1915e16ee48d85386c4d9e7e3ad4eab3d6
soulgalore <peter@soulgalore.com>
2016-03-22 08:22:02 +0100

    add domAdvice and options to each test

--- test/help/har.js
+++ test/help/har.js
@@ -8,10 +8,10 @@ let fs = require('fs'),
 Promise.promisifyAll(fs);
 
 module.exports = {
-  getHARresult: function(harFile) {
+  getHARresult: function(harFile, domAdvice, options) {
     return fs.readFileAsync(path.resolve(harFile))
       .then(JSON.parse)
-      .then((har) => api.runHarAdvice(har, api.getHarAdvice()));
+      .then((har) => api.runHarAdvice(har, api.getHarAdvice(), domAdvice, options));
   },
   getHAR: function(harFile) {
     return fs.readFileAsync(path.resolve(harFile))
