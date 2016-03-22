220fa4c8a2d70a60f94126782edee2df225b9040
soulgalore <peter@soulgalore.com>
2016-03-23 00:11:28 +0100

    we also check HTML

--- test/har/performance/privateAssetsTest.js
+++ test/har/performance/privateAssetsTest.js
@@ -6,7 +6,7 @@ let har = require('../../help/har');
 describe('Avoid setting private headers on items that can be cached', function() {
   it('We should be able to find cache headers that are private', function() {
     return har.getHARresult('test/har/files/privateAssets.har').then((result) => {
-      assert.strictEqual(result[0].coachAdvice.results.performance.adviceList.privateAssets.offending.length, 22);
+      assert.strictEqual(result[0].coachAdvice.results.performance.adviceList.privateAssets.offending.length, 31);
       assert.strictEqual(result[0].coachAdvice.results.performance.adviceList.privateAssets.score, 0);
     });
   });
