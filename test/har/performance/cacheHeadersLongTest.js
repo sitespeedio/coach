4251612be9e74ac780b88abb24bbf5a2a185869d
XhmikosR <xhmikosr@gmail.com>
2016-06-07 10:10:59 +0300

    Fix tests after 7f716456ae69585c4cbfdabe96de4a2177d0a147. (#128)

--- test/har/performance/cacheHeadersLongTest.js
+++ test/har/performance/cacheHeadersLongTest.js
@@ -6,9 +6,9 @@ let har = require('../../help/har');
 describe('Use cache headers', function() {
   it('We should be able to know if cache times are too short', function() {
     return har.firstAdviceForTestFile('cacheHeaders.har').then((result) => {
-      assert.strictEqual(result.performance.adviceList.cacheHeadersLong.score, 68);
+      assert.strictEqual(result.performance.adviceList.cacheHeadersLong.score, 70);
       // massive
-      assert.strictEqual(result.performance.adviceList.cacheHeadersLong.offending.length, 32);
+      assert.strictEqual(result.performance.adviceList.cacheHeadersLong.offending.length, 30);
     });
   });
 
