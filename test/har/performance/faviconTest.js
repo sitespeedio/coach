a025a878e6f27c230d8c90216ad9b7ea9ca045b6
Peter Hedenskog <phedenskog@wikimedia.org>
2018-11-15 13:19:43 +0100

    fixed tests to follow new structure

--- test/har/performance/faviconTest.js
+++ test/har/performance/faviconTest.js
@@ -6,16 +6,8 @@ let har = require('../../help/har');
 describe('Use favicon', function() {
   it('We should be able to know if we have a favicon', function() {
     return har.firstAdviceForTestFile('favicon.har').then(result => {
-      assert.strictEqual(result.performance.adviceList.favicon.score, 100);
+      assert.strictEqual(result.performance.adviceList.favicon.score, 50);
     });
   });
 
-  it('We should be able to know if we we are missing favicon', function() {
-    // The favicon advice only works correct for Chrome
-    return har
-      .firstAdviceForTestFile('favicon2.har', { browser: 'chrome' })
-      .then(result => {
-        assert.strictEqual(result.performance.adviceList.favicon.score, 0);
-      });
-  });
 });
